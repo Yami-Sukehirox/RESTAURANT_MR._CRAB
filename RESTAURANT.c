1df447035ddeafe0833a745364c3f0110cc9f491
unknown <liancruspero1@gmail.com>
2026-09-05 09:37:17 +0800

    NEW CODE

---
+++ RESTAURANT.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main () {
+	
+	int choice, quantity, again;
+	float total = 0;
+	float payment, change;
+	
+	do {
+	
+	printf ("\n==========MR. CRAB RESTAURANT============\n");
+	
+	printf ("\n-----MENU-------\n");
+	printf ("\n1. Garlic Butter");
+	printf ("\n2. Mud Crabs");
+	printf ("\n3. Butter Shirmp");
+	printf ("\n4. Sizzling Squid");
+	printf ("\n5. Crispy Pata");
+	printf ("\n5. Checkout");
+	
+	printf ("\nEnter your order: ");
+	scanf("%d", &choice);
+	
+	switch(choice) {
+		case 1:
+			printf ("Quantity ");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
